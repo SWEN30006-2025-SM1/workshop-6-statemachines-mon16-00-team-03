5263d7e04beadb27b4c667ea6afab09c8f4513f4
Elizabeth Pham <ehpham@student.unimelb.edu.au>
2025-04-14 19:47:23 +1000

    outside states

--- app/src/main/java/com/unimelb/swen30006/workshops/Account.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Account.java
@@ -2,8 +2,30 @@ package com.unimelb.swen30006.workshops;
 
 public class Account {
     State state;
+    int currentCardNumber;
+
+    private State accountState;
 
     public void onboard() {
         State state = State.PENDING;
     }
+
+    //public void activateCard(){}
+
+
+    public void closeAccount(){
+        accountState = State.CLOSED;
+    }
+
+    public void activateAccount(){
+        accountState = State.ACTIVE;
+    }
+    public void inactivateAccount(){
+        accountState = State.INACTIVE;
+    }
+    public void suspendAccount(){
+        accountState = State.SUSPENDED;
+    }
 }
+
+
